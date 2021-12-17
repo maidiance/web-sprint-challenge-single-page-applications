7e51a9d2ae53ceee571e4cc0d1e4b1a67b588724
Winifred Lu <winnie.lu@comcast.net>
2021-12-17 13:59:18 -0500

    elements exist test works

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -5,17 +5,17 @@ describe('Pizza App', () => {
 
     // helpers to grab elements
     const nameInput = () => cy.get('input[name=nameInput]');
-    const pizzaSizeInput = () => cy.get('input[name=sizeDropdown]');
+    const pizzaSizeInput = () => cy.get('#size-dropdown');
     const toppingsInput = () => cy.get('input[type="checkbox"]');
-    const specialInstructionsInput = () => cy.get('input[name=specialInstructions]');
+    const specialTextInput = () => cy.get('input[name=specialText]');
     const submitButton = () => cy.get('button[id="order-button"]');
 
     it('proper elements are showing', () => {
         nameInput().should('exist');
         pizzaSizeInput().should('exist');
         toppingsInput().should('exist');
-        specialInstructionsInput().should('exist');
+        specialTextInput().should('exist');
         submitButton().should('exist');
     })
-    
+
 })
