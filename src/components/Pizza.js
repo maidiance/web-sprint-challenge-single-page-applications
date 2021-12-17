7284e6578b951a5025ff173f6bb5a25d549696c4
Winifred Lu <winnie.lu@comcast.net>
2021-12-17 12:57:21 -0500

    pizza element complete for now

---
+++ src/components/Pizza.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Pizza({ details }) {
+    if (!details) {
+        return <h3>Working fetching your pizza orders...</h3>
+    }
+
+    return (
+        <div className='pizza container'>
+            <h2>{details.name}</h2>
+            <p>Pizza size: {details.pizzaSize}</p>
+            {
+                !!details.toppings && !!details.toppings.length &&
+                <div>
+                    Toppings:
+                    <ul>
+                        {details.toppings.map((topping, idx) => <li key={idx}>{topping}</li>)}
+                    </ul>
+                </div>
+            }
+            <p>Special Instructions: {details.specialInstructions}</p>
+        </div>
+    )
+}
+
+export default Pizza;
